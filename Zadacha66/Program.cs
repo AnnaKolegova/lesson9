37b48807c0bb26e1ebef75fad1a8e14027afed83
Anna <eekolegov@mail.ru>
2022-10-14 21:45:09 +0300

    Zadacha66

---
+++ Zadacha66/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число");
+int n=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int m=Convert.ToInt32(Console.ReadLine());
+int sum=0;
+Console.WriteLine(FillNumber(m,sum, n));
+int FillNumber(int m, int sum, int a=1)
+{
+    sum=sum+a;
+    a++;
+    if (a>m)
+    {
+        return sum;
+    }
+    return FillNumber(m, sum, a);
+  
+
+}
+   
