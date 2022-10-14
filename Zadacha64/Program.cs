32685b406ce4eaa7e2b17e26ed73689f9b840643
Anna <eekolegov@mail.ru>
2022-10-14 21:28:09 +0300

    Zadacha64

---
+++ Zadacha64/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число");
+int n=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int m=Convert.ToInt32(Console.ReadLine());
+FillNumber(n,m);
+void FillNumber(int n, int m)
+{
+    if (n%3==0)&& (n>=m);
+    {
+        return;
+    }
+    FillNumber(n, m);
+  
+
+}
